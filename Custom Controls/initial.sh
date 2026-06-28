dbeee4379a60d932a0faaa9045fa8192adeda77d
MTrethowan <79941488+MTrethowan@users.noreply.github.com>
2026-06-28 15:14:30 -0700

    Delete Custom Controls directory

--- Custom Controls/initial.sh
+++
@@ -1,6 +0,0 @@
-git init
-git branch -M main
-git remote add origin https://github.com/MTrethowan/Rotary-Knob-Control-For-.Net-Forms.git
-git add .
-git commit -m "Initial commit"
-git push -u origin main
